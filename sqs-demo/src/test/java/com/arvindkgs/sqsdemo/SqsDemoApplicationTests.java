64c6cec316c1e44fa94677dc24b62df4c61db5a2
arvindkgs <arvindkgs@nile-global.com>
2021-07-31 19:42:58 +0530

    SQS Demo init

---
+++ sqs-demo/src/test/java/com/arvindkgs/sqsdemo/SqsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arvindkgs.sqsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
